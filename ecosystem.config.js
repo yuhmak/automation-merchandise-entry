f1f9072c05925b98671a6221f04890310c7d8f02
Gaston Edgardo Rocha <96696302+Chinchuwiki@users.noreply.github.com>
2026-03-20 19:38:46 -0300

    Add cron_restart to automation-merchandise config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'automation-merchandise',
       script: './index.js',
       instances: 1,
+      cron_restart: "*/10 * * * *",
       autorestart: false,
       watch: false,
       max_memory_restart: '600M',
